284ce098eb2664bd75b89f815c80c680272a8b53
Wade West <wwest81@gmail.com>
2012-01-12 08:03:25 -0600

    Disabled auto update

--- current-rc.zsh
+++ current-rc.zsh
@@ -10,7 +10,7 @@ ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="darkblood"
 
 # Set to this to use case-sensitive completion
-# CASE_SENSITIVE="true"
+CASE_SENSITIVE="true"
 
 # Comment this out to disable weekly auto-update checks
 # DISABLE_AUTO_UPDATE="true"
